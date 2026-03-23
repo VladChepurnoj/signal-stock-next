dcd6838e677b0adf4f0d6be6ab09423b59c603e5
Vlad <chepurnojv@gmail.com>
2026-03-23 07:01:56 +0200

    daily summary ingest main

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,11 +1,14 @@
 import nodemailer from "nodemailer";
-import { WELCOME_EMAIL_TEMPLATE } from "@/lib/nodemailer/templates";
+import {
+  WELCOME_EMAIL_TEMPLATE,
+  NEWS_SUMMARY_EMAIL_TEMPLATE,
+} from "@/lib/nodemailer/templates";
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: process.env.MDOEMAILER_EMAIL,
-    pass: process.env.NODEMAILER_PASSWORD,
+    user: process.env.NODEMAILER_EMAIL!,
+    pass: process.env.NODEMAILER_PASSWORD!,
   },
 });
 
@@ -20,10 +23,35 @@ export const sendWelcomeEmail = async ({
   );
 
   const mailOptions = {
-    from: `"Signalist <signallist@jsmastery.pro>"`,
+    from: `"Signalist" <signalist@jsmastery.pro>`,
     to: email,
-    subject: "Welcome to Signalist - your stock market toolkit is ready",
-    test: "Thanks for joining Signalist",
+    subject: `Welcome to Signalist - your stock market toolkit is ready!`,
+    text: "Thanks for joining Signalist",
+    html: htmlTemplate,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+export const sendNewsSummaryEmail = async ({
+  email,
+  date,
+  newsContent,
+}: {
+  email: string;
+  date: string;
+  newsContent: string;
+}): Promise<void> => {
+  const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE.replace(
+    "{{date}}",
+    date,
+  ).replace("{{newsContent}}", newsContent);
+
+  const mailOptions = {
+    from: `"Signalist News" <signalist@jsmastery.pro>`,
+    to: email,
+    subject: `📈 Market News Summary Today - ${date}`,
+    text: `Today's market news summary from Signalist`,
     html: htmlTemplate,
   };
 
